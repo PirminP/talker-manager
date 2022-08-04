e2862e3b5d6179977bb773e52d2199ef56514b68
PirminP <pirmin.pernsteiner@gmail.com>
2022-08-04 17:52:15 -0300

    feat: Create endpoint GET /talker

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-// Let's code!!!
 const express = require('express');
+const fs = require('fs/promises');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const PORT = '3002';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+async function getTalkers() {
+  const talkers = JSON.parse(await fs.readFile('./talker.json', 'utf8'));
+  return talkers;
+}
+
+app.get('/talker', async (req, res) => {
+  try {
+    const talkers = await getTalkers();
+    return res.status(200).json(talkers);
+  } catch (error) {
+    return res.status(200).json([]);
+  }
 });
 
 app.listen(PORT, () => {
